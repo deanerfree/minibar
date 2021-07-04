a38882e4c38e201a9bc28865a1346ed9f49c3839
Kurt Ball <kurtisdball@hotmail.com>
2021-07-04 13:46:08 -0600

    playing with connections

--- server/plugins/config.js
+++ server/plugins/config.js
@@ -1,30 +1,32 @@
-const { Connection, Request } = require("tedious");
+// const { Connection, Request } = require("tedious");
+// const sql = require("mssql");
 // Create connection to database
-const config = {
-	authentication: {
-		options: {
-			userName: "zeus", // update me
-			password: "0lympi@!", // update me
-		},
-		type: "default",
-	},
-	server: "coril-test-sql-server.database.windows.net", // update me
-	options: {
-		database: "KurtTest", //update me
-		encrypt: true,
-	},
-};
+// const config = {
+// 	authentication: {
+// 		options: {
+// 			userName: "zeus", // update me
+// 			password: "0lympi@!", // update me
+// 		},
+// 		type: "default",
+// 	},
+// 	server: "coril-test-sql-server.database.windows.net", // update me
+// 	options: {
+// 		database: "KurtTest", //update me
+// 		encrypt: true,
+// 	},
+// };
+config =
+	"Server=coril-test-sql-server.database.windows.net;Database=KurtTest;User Id=zeus;Password=0lympi@!;Encrypt=true";
+// const connection = new Connection(config);
+// // Attempt to connect and execute queries if connection goes through
+// connection.on("connect", (err) => {
+// 	if (err) {
+// 		console.error(err.message);
+// 	} else {
+// 		console.log("Connected to Azure App Service");
+// 		// queryDatabase();
+// 	}
+// });
+// connection.connect();
 
-const connection = new Connection(config);
-// Attempt to connect and execute queries if connection goes through
-connection.on("connect", (err) => {
-	if (err) {
-		console.error(err.message);
-	} else {
-		console.log("Connected to Azure App Service");
-		// queryDatabase();
-	}
-});
-connection.connect();
-
-module.exports = connection;
+module.exports = config;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,28 +1,32 @@
 const router = require("express").Router();
-const { Request } = require("tedious");
-const connection = require("../plugins/config");
+const sql = require("mssql");
+// const { Request } = require("tedious");
+const config = require("../plugins/config");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
 	try {
-		const queryDatabase = () => {
-			console.log("Reading rows from the Table...");
-			// Read all rows from table
-			const sql = `SELECT * FROM dbo.drinks;`;
-			const request = new Request(sql, (err, rowCount) => {
-				if (err) {
-					console.error(err.message);
-				} else {
-					console.log(`${rowCount} row(s) returned`);
-				}
-			});
-			request.on("row", (columns) => {
-				columns.forEach((column) => {
-					console.log("%s\t%s", column.metadata.colName, column.value);
-				});
-			});
-			connection.execSql(request);
-		};
-		res.status(200).send(queryDatabase());
+		await sql.connect(config);
+		result = await sql.query`SELECT * FROM dbo.drinks;`;
+		console.log("This is the result:", result);
+		// const queryDatabase = () => {
+		// 	console.log("Reading rows from the Table...");
+		// 	// Read all rows from table
+		// 	const sql = `SELECT * FROM dbo.drinks;`;
+		// 	const request = new Request(sql, (err, rowCount) => {
+		// 		if (err) {
+		// 			console.error(err.message);
+		// 		} else {
+		// 			console.log(`${rowCount} row(s) returned`);
+		// 		}
+		// 	});
+		// 	request.on("row", (columns) => {
+		// 		columns.forEach((column) => {
+		// 			console.log("%s\t%s", column.metadata.colName, column.value);
+		// 		});
+		// 	});
+		// 	connection.execSql(request);
+		// };
+		res.status(200).send(result);
 	} catch (error) {
 		console.error("This is the Error:", error);
 	}
